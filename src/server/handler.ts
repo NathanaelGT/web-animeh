3613b174f1a929181ea0cd827c714b81bad3ca39
NathanaelGT <gordontanu2004@gmail.com>
2024-08-13 12:35:12 +0800

    refactor: Memisahkan logic upgrade websocket

--- src/server/handler.ts
+++ src/server/handler.ts
@@ -5,11 +5,8 @@ import { createBunWSHandler } from 'trpc-bun-adapter'
 import { createTRPCContext } from '~s/trpc'
 import { TRPCRouter } from '~s/router'
 // import { safePath } from '~s/utils/path'
-import { db } from '~s/db'
-import { profiles } from '~s/db/schema'
-import { defaultSettings, parse } from '~/shared/profile/settings'
+import { handleWebsocketRequest } from '~s/http-handler/websocket'
 import { isProduction } from '~s/env' with { type: 'macro' }
-import type { WebSocketData } from '~s/index'
 
 let indexHtml: Buffer
 
@@ -26,68 +23,12 @@ export const websocket = createBunWSHandler({
   allowBatching: false,
 })
 
-const globalForId = globalThis as unknown as {
-  id: number
-}
-if (!isProduction()) {
-  globalForId.id ??= 1
-}
-
-let id = 1
-
 export const httpHandler = async (
   request: Request,
   server: Bun.Server,
 ): Promise<undefined | Response> => {
   if (request.headers.get('upgrade') === 'websocket') {
-    const profileId = Number(request.url.slice(server.url.origin.length + 1))
-
-    let profile = await db.query.profiles.findFirst({
-      where(profiles, { eq }) {
-        // baru pertama kali, belum ada profil yang dipilih
-        if (!profileId) {
-          return undefined
-        }
-
-        return eq(profiles.id, profileId)
-      },
-    })
-
-    // ada kemungkinan profile yang terakhir dipake dihapus dari db
-    if (profile === undefined) {
-      profile = await db.query.profiles.findFirst()
-    }
-
-    if (profile === undefined) {
-      // table profiles kosong
-      const newProfile: typeof profiles.$inferInsert = {
-        name: 'Default',
-        settings: defaultSettings(),
-      }
-
-      const results = await db.insert(profiles).values(newProfile).returning({ id: profiles.id })
-
-      newProfile.id = results[0]!.id
-
-      profile = newProfile as typeof profiles.$inferSelect
-    } else {
-      profile.settings = parse(profile.settings)
-    }
-
-    const upgradeSuccess = server.upgrade(request, {
-      data: {
-        id: (isProduction() ? id : globalForId.id).toString(36),
-        profile,
-      } satisfies WebSocketData,
-    })
-
-    if (upgradeSuccess) {
-      if (isProduction()) {
-        id++
-      } else {
-        globalForId.id++
-      }
-
+    if (await handleWebsocketRequest(request, server)) {
       return
     }
   }
---
+++ src/server/http-handler/websocket.ts
@@ -0,0 +1,68 @@
+import Bun from 'bun'
+import { db } from '~s/db'
+import { profiles } from '~s/db/schema'
+import { defaultSettings, parse } from '~/shared/profile/settings'
+import { isProduction } from '~s/env' with { type: 'macro' }
+import type { WebSocketData } from '~s/index'
+
+const globalForId = globalThis as unknown as {
+  id: number
+}
+if (!isProduction()) {
+  globalForId.id ??= 1
+}
+
+let id = 1
+
+export const handleWebsocketRequest = async (request: Request, server: Bun.Server) => {
+  const profileId = Number(request.url.slice(server.url.origin.length + 1))
+
+  let profile = await db.query.profiles.findFirst({
+    where(profiles, { eq }) {
+      // baru pertama kali, belum ada profil yang dipilih
+      if (!profileId) {
+        return undefined
+      }
+
+      return eq(profiles.id, profileId)
+    },
+  })
+
+  // ada kemungkinan profile yang terakhir dipake dihapus dari db
+  if (profile === undefined) {
+    profile = await db.query.profiles.findFirst()
+  }
+
+  if (profile === undefined) {
+    // table profiles kosong
+    const newProfile: typeof profiles.$inferInsert = {
+      name: 'Default',
+      settings: defaultSettings(),
+    }
+
+    const results = await db.insert(profiles).values(newProfile).returning({ id: profiles.id })
+
+    newProfile.id = results[0]!.id
+
+    profile = newProfile as typeof profiles.$inferSelect
+  } else {
+    profile.settings = parse(profile.settings)
+  }
+
+  const upgradeSuccess = server.upgrade(request, {
+    data: {
+      id: (isProduction() ? id : globalForId.id).toString(36),
+      profile,
+    } satisfies WebSocketData,
+  })
+
+  if (upgradeSuccess) {
+    if (isProduction()) {
+      id++
+    } else {
+      globalForId.id++
+    }
+  }
+
+  return upgradeSuccess
+}
