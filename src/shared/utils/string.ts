5718345750266f50ebbc80d4c256bdb854e563d1
NathanaelGT <gordontanu2004@gmail.com>
2024-09-01 23:17:03 +0800

    chore: Menambahkan helper function string `before`

--- src/shared/utils/string.ts
+++ src/shared/utils/string.ts
@@ -9,3 +9,13 @@ export const ucFirst = (str: string) => {
 export const kebabCaseToTitleCase = (str: string) => {
   return str.split('-').map(ucFirst).join(' ')
 }
+
+export const before = (str: string, search: string) => {
+  const untilIndex = str.indexOf(search)
+
+  if (untilIndex > -1) {
+    return str.slice(0, untilIndex)
+  }
+
+  return str
+}
