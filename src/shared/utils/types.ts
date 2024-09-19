818ca7db313c42ec61e64519f5a79ffda7ed4fe4
NathanaelGT <gordontanu2004@gmail.com>
2024-09-19 08:34:46 +0800

    chore: Create `TRPCParams` type helper

--- src/shared/utils/types.ts
+++ src/shared/utils/types.ts
@@ -1,5 +1,7 @@
 import type { AnyProcedure } from '@trpc/server/unstable-core-do-not-import'
 
+export type TRPCParams<TProcedure extends AnyProcedure> = TProcedure['_def']['$types']['input']
+
 export type TRPCResponse<TProcedure extends AnyProcedure> = TProcedure['_def']['$types']['output']
 
 export type NumericKeys<T extends readonly unknown[]> = Exclude<keyof T, keyof any[]>
