cd13a30c1eff1a9f38c6577b0c3a89a35f7b070c
NathanaelGT <gordontanu2004@gmail.com>
2024-10-18 23:06:52 +0800

    chore: Create `randomBetween` helper for number

--- src/shared/utils/number.ts
+++ src/shared/utils/number.ts
@@ -11,3 +11,7 @@ export const parseNumber = (value: any) => {
 export const clamp = (number: number, min = -Infinity, max = Infinity) => {
   return Math.min(Math.max(number, min), max)
 }
+
+export const randomBetween = (min: number, max: number) => {
+  return Math.round(Math.random() * (max - min) + min)
+}
