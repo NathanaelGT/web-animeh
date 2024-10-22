cb104e731bf4560ce8471c1df98e70bf79587854
NathanaelGT <gordontanu2004@gmail.com>
2024-10-22 22:25:27 +0800

    ui: Change skeleton default border radius

--- src/client/components/ui/skeleton.tsx
+++ src/client/components/ui/skeleton.tsx
@@ -1,7 +1,7 @@
 import { cn } from '~c/utils'
 
 function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn('animate-pulse rounded-md bg-muted', className)} {...props} />
+  return <div className={cn('animate-pulse rounded-sm bg-muted', className)} {...props} />
 }
 
 export { Skeleton }
