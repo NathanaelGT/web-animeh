38b8f97063bff615cf5dcd1d0c095bffd73f3677
NathanaelGT <gordontanu2004@gmail.com>
2024-10-28 21:37:52 +0800

    feat: Anime title tooltip

--- src/client/components/Anime/Title.tsx
+++ src/client/components/Anime/Title.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Link } from '@tanstack/react-router'
 import { api } from '~/client/trpc'
+import { Image } from '@/Image'
 import { toast } from '@/ui/use-toast'
 import {
   ContextMenu,
@@ -12,7 +13,13 @@ import {
   ContextMenuSubContent,
   ContextMenuSubTrigger,
 } from '@/ui/context-menu'
-import { SimpleTooltip } from '@/ui/tooltip'
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipArrow,
+} from '@/ui/tooltip'
 import type { ComponentProps } from 'react'
 import type { AnimeData as BaseAnimeData } from '~c/stores'
 
@@ -71,7 +78,7 @@ export function AnimeTitle<TTag extends Tag = 'p', TAsLink extends boolean = fal
     animeData.title
   )
 
-  text = withTooltip ? <SimpleTooltip title={animeData.title}>{text}</SimpleTooltip> : text
+  text = withTooltip ? <TitleTooltip animeData={animeData}>{text}</TitleTooltip> : text
 
   return (
     <ContextMenu>
@@ -105,6 +112,87 @@ export function AnimeTitle<TTag extends Tag = 'p', TAsLink extends boolean = fal
         </ContextMenuContent>
       )}
     </ContextMenu>
+  )
+}
+
+type TitleTooltipProps = {
+  animeData: AnimeData
+  children: string | JSX.Element
+}
+
+function TitleTooltip({ animeData, children }: TitleTooltipProps) {
+  const [shouldQueryPreview, setShouldQueryPreview] = useState(false)
+  const previewQuery = api.component.animeTitle.preview.useQuery(animeData.id, {
+    enabled: shouldQueryPreview,
+  })
+
+  const onTooltipChangeOpen = (isOpen: boolean) => {
+    if (!shouldQueryPreview && isOpen) {
+      setShouldQueryPreview(true)
+    }
+  }
+
+  const { data } = previewQuery
+
+  return (
+    <TooltipProvider>
+      <Tooltip onOpenChange={onTooltipChangeOpen}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        {data && (
+          <TooltipContent className="max-w-lg rounded-md bg-popover p-0 text-popover-foreground">
+            <div className="bg-muted px-5 py-3">
+              <Link
+                to="/anime/$id"
+                params={{ id: animeData.id.toString() }}
+                className="text-lg font-semibold"
+              >
+                {animeData.title}
+              </Link>
+            </div>
+
+            <div className="grid grid-cols-[auto_1fr] gap-x-5 gap-y-3 p-5 text-sm">
+              <Link to="/anime/$id" params={{ id: animeData.id.toString() }} tabIndex={-1}>
+                <Image
+                  src={animeData.id}
+                  className="h-[220px] w-[156px] rounded-md bg-muted object-cover shadow outline outline-1 outline-slate-600/20"
+                />
+              </Link>
+              {data.synopsis ? (
+                <p
+                  className="line-clamp-1 whitespace-pre text-pretty text-justify"
+                  // line-height dari text-sm = 1.25rem = 20px, height gambar = 220px
+                  // arbitrary value dari tailwind ngebug, gabisa pake line-clamp-[11]
+                  style={{ WebkitLineClamp: 11 }}
+                >
+                  {data.synopsis.replaceAll('\n\n', '\n')}
+                </p>
+              ) : (
+                <p className="text-muted-foreground">Tidak ada sinopsis</p>
+              )}
+
+              <div className="col-span-2 grid gap-1">
+                <div>
+                  <span className="font-bold">Episode</span>: {data.currentEpisode}/
+                  {data.totalEpisodes ?? '?'}
+                </div>
+
+                <div className="flex flex-wrap gap-x-2 gap-y-1">
+                  <span>
+                    <span className="font-bold">Genre</span>:{' '}
+                  </span>
+                  {data.genres.map(genre => (
+                    <span key={genre} className="rounded-full border border-primary/40 px-2">
+                      {genre}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <TooltipArrow className="-mt-[1px] fill-popover" />
+          </TooltipContent>
+        )}
+      </Tooltip>
+    </TooltipProvider>
   )
 }
 
--- src/server/trpc-procedures/components/anime-title.ts
+++ src/server/trpc-procedures/components/anime-title.ts
@@ -1,8 +1,48 @@
 import * as v from 'valibot'
 import { procedure, router } from '~s/trpc'
 import { getKuramanimeOrigin } from '~/server/ky'
+import { omit } from '~/shared/utils/object'
 
 export const AnimeTitleRouter = router({
+  preview: procedure.input(v.parser(v.number())).query(async ({ ctx, input }) => {
+    const animeData = await ctx.db.query.anime.findFirst({
+      where: (anime, { eq }) => eq(anime.id, input),
+      columns: {
+        synopsis: true,
+        totalEpisodes: true,
+      },
+      with: {
+        animeToGenres: {
+          columns: {},
+          with: {
+            genre: {
+              columns: {
+                name: true,
+              },
+            },
+          },
+        },
+        episodes: {
+          columns: {
+            number: true,
+          },
+          orderBy: (episodes, { desc }) => desc(episodes.number),
+          limit: 1,
+        },
+      },
+    })
+
+    if (!animeData) {
+      return null
+    }
+
+    return {
+      ...omit(animeData, 'animeToGenres', 'episodes'),
+      currentEpisode: animeData.episodes[0]?.number ?? 0,
+      genres: animeData.animeToGenres.map(({ genre }) => genre.name),
+    }
+  }),
+
   context: procedure.input(v.parser(v.number())).query(async ({ ctx, input }) => {
     const [kuramanimeOrigin, metadata] = await Promise.all([
       getKuramanimeOrigin(),
