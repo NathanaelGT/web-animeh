81cbc670be6482e72db9d3942ca6355e4ecd8fed
NathanaelGT <gordontanu2004@gmail.com>
2026-02-19 12:04:11 +0700

    perf: Improve `parseMalId` performance

--- src/server/utils/mal.ts
+++ src/server/utils/mal.ts
@@ -1,5 +1,5 @@
 export const parseMalId = (malUrl: string) => {
   const slicedMalUrl = malUrl.slice('https://myanimelist.net/anime/'.length)
 
-  return Number(slicedMalUrl?.slice(0, slicedMalUrl.indexOf('/')))
+  return parseInt(slicedMalUrl)
 }
